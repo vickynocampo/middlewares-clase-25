3228a99dd31c579a1f93ba52c05c299f0d823dcd
vickynocampoemail <you@example.com>
2022-07-14 09:48:45 -0300

    funcion para pasar array y variable a loweCase

--- src/middlewares/adminMiddleware.js
+++ src/middlewares/adminMiddleware.js
@@ -1,14 +1,30 @@
-const fs = require("fs");
-const path = require("path");
 
 function adminMiddleware(req, res, next) {
     let queryUser = req.query.user;
     let adminUsers = ["Ada", "Greta", "Vim", "Tim"];
-   if(adminUsers.includes(queryUser)){
-    next();
-   }else{ 
-    res.send("No tienes los privilegios para ingresar")
-   }
+
+    /*const adminUsersLowerCase =  adminUsers.map((usuario) => usuario.toLowerCase())*/
+
+    if (adminUsers
+        .map((usuario) => usuario.toLowerCase())
+        .includes(queryUser.toLowerCase())
+    ) {
+        next();
+    } else {
+        res.send("No tienes los privilegios para ingresar")
+    };
+
+    /*let isUserFinded = false;
+    for (let i = 0; i < adminUsers.length; i++) {
+        if (adminUsers[i].toLowerCase() === queryUser.toLowerCase()) {
+            isUserFinded = true;
+        }
+    }
+    if (isUserFinded) {
+        next();
+    } else {
+        res.send("No tienes los privilegios para ingresar")
+    }*/
 }
 
 module.exports = adminMiddleware;
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,11 +1,10 @@
 // ************ Require's ************
 const express = require('express');
 const router = express.Router();
-const logMiddleware = require("../middlewares/logMiddleware")
 
 // ************ Controller Require ************
 const mainController = require('../controllers/mainController');
 
-router.get('/', logMiddleware, mainController.index); //
+router.get('/', mainController.index); //
 
 module.exports = router;
